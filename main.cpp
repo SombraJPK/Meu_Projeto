9a555cc062ab359d71b5765224197afc16aeac76
Sombra <Sombrajpk@hotmail.com>
2020-11-24 15:34:31 -0300

    Codigo fonte do arquivo foi modificado para uma função simples, que reconhece se Valor X é maior ou menos que Y

--- main.cpp
+++ main.cpp
@@ -5,9 +5,21 @@ using namespace std;
 int main()
 {
 
-    cout<<"TEST!!!"<<endl;
-    int 300;
-    int b;
+int num1,num2;
+cout<<"Digite um numero: ";
+cin>>num1;
+cout<<"Digite outro numero: ";
+cin>>num2; 
+cout<<"\n\nPrimeiro numero digitado foi: "<<num1<<"\nSegundo numero digitado foi: "<<num2<<endl;
+
+if (num1<num2)
+{
+	cout<<"Segundo valor digitado e maior que o primeiro"<<endl;
+}
+else
+{
+	cout<<"Primeiro valor digitado e maior que o segundo valor digitado "<<endl;
+}
 
 	return 0;
 }
